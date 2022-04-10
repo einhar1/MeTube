dabd060ec50ffd655a6682c3374da7a604601092
einhar1 <77612217+einhar1@users.noreply.github.com>
2022-04-10 19:36:07 +0200

    lagt till loginsidan, men funkar inte än så länge

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var request;
 
 $(document).ready(function () {
+
+    // Denna del innehåller kod kopierad från diverse sidor.
     $(document).on('submit', '#searchbar', function () {
         
         if (window.XMLHttpRequest) {
@@ -19,5 +21,34 @@ $(document).ready(function () {
 
         return false;
     });
-    
+
+    // Get the modal
+    var modal = document.getElementById('id01');
+    var close = document.getElementById('close');
+    var cancelbtn = document.getElementById('cancelbtn');
+    var link = document.getElementById('link');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            document.getElementById('body').style.overflow = 'auto';
+        }
+    }
+
+    close.onclick = function (event) {
+        document.getElementById('id01').style.display = 'none';
+        document.getElementById('body').style.overflow = 'auto';
+    }
+
+    cancelbtn.onclick = function (event) {
+        document.getElementById('id01').style.display = 'none';
+        document.getElementById('body').style.overflow = 'auto';
+    }
+
+    link.onclick = function (event) {
+        document.getElementById('id01').style.display = 'block';
+        document.getElementById('body').style.overflow = 'hidden';
+    }
+
 });
